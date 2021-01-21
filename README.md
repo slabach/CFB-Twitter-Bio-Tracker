01aeeb8b0cf48ad41ad5bf93b614e52201234103
slabach <byron.slabach@gmail.com>
2021-01-21 01:40:53 +0000

    move sensitive data to config.json

---
+++ README.md
@@ -0,0 +1,25 @@
+# College Football Twitter Bio Update Script
+
+### Run this from the command line. 
+### This script will run every 5 minutes for as many cycles as specified in Command Line arg
+### On first cycle, it will gather each users (from the 'twthandle' variable) bio,
+### First and all subsequent cycles will then check to see if the users bio has changed. If it has,
+### it will direct message any user specified ('myself' variable) with the updated bio
+
+### Used to track potential coaching changes in CFB offseason
+
+### Example:
+    'python3 main.py 2'
+        - this will run 2 cycles. It will run once, wait 5 minutes, run again, then quit.
+
+### Arguments:
+    api_secret = Twitter Api Secret
+    api_token = Twitter Api Token
+    consumer_key = Twitter Api Consumer Key
+    consumer_secret = Twitter Api Consumer Secret
+
+    twthandle = list of Twitter handles to watch
+    myself = your own twitter handle to DM if there are any updates
+
+### Returns:
+    none
---
+++ main.py
@@ -0,0 +1,61 @@
+import tweepy
+import time, sys, requests, json
+
+base_url = 'https://perfectfall.com/api/system-property'
+
+
+with open("config.json", "r") as c:
+    config = json.load(c)
+
+base_url = 'https://perfectfall.com/api/system-property'
+
+tw_api_secret = requests.get(f'{base_url}/40', headers={'token': config['api-key']}).text
+tw_api_token = requests.get(f'{base_url}/41', headers={'token': config['api-key']}).text
+tw_consumer_key = requests.get(f'{base_url}/42', headers={'token': config['api-key']}).text
+tw_consumer_sec = requests.get(f'{base_url}/43', headers={'token': config['api-key']}).text
+
+tw_api_secret_j = json.loads(tw_api_secret)
+tw_api_token_j = json.loads(tw_api_token)
+tw_consumer_key_j = json.loads(tw_consumer_key)
+tw_consumer_sec_j = json.loads(tw_consumer_sec)
+
+api_secret = tw_api_secret_j['value']
+api_token = tw_api_token_j['value']
+consumer_key = tw_consumer_key_j['value']
+consumer_secret = tw_consumer_sec_j['value']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(api_token, api_secret)
+api = tweepy.API(auth)
+
+myself = config['user']
+my_ins = api.get_user(myself)
+counter = 0
+max_rep = int(sys.argv[1])
+print(f"Cycles to run: {max_rep}")
+api.send_direct_message(my_ins.id, 'test')
+
+twthandle = config['handles']
+bio = []
+for i in range(len(twthandle)):
+    user = api.get_user(twthandle[i])
+    bio.append(user.description)
+    msg = f'START: {twthandle[i]}\n\n{user.description}'
+    # print(msg)
+    # api.send_direct_message(my_ins.id, msg)
+
+# 12 per hr
+while counter < max_rep:
+    for i in range(len(twthandle)):
+        user = api.get_user(twthandle[i])
+        b = user.description
+        if bio[i] != b:
+            msg = f"CHANGE:\n\n@{twthandle[i]} changed their Twitter Bio from: '{bio[i]}' to '{b}'."
+            api.send_direct_message(my_ins.id, msg)
+            bio[i] = b
+            print(msg)
+    time.sleep(300)
+    counter = counter + 1
+    # print(f"")
+
+print(f"{max_rep + 1} cycles completed")
